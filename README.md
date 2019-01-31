9576feab27ab7aa35829a6b4df7d135a868bb170
Alex Clever <alex.clever@signalayer.com>
2019-01-31 18:54:09 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# react-signalayer-player
+To go live and start displaying campaigns to users, you'll need to install Signalayer Player script on your site.
+
+## Installation
+```bash
+npm i react-signalayer-player --save
+```
+
+## Usage
+Inside of your application where you would be running Signalayer, insert `SignalayerPlayer`:
+```js
+import React from 'react';
+import SignalayerPlayer from 'react-signalayer-player';
+
+export class App extends React.Component {
+
+  render () {
+    const userData = {
+        userId: "123",
+        email: "user@email.com"
+    };
+
+    return (
+      <div className="app">
+        <SignalayerPlayer projectId="..." async={false} userData={userData} />
+      </div>
+    );
+  }
+}
+```
+
+#### API Usage
+Player API is a developer interface to communicate your site's data to Signalayer. The API will help you set up custom triggers and personalize campaigns based on user data and actions.
+
+Be sure to Install **Signalayer Player** on your site before using the API.
+
+```js
+import { SignalayerAPI } from 'react-signalayer-player';
+SignalayerAPI.show("campaign_id");
+```
+
+
+Find more information [Docs](https://signalayer.com/docs#apiDocsSection)
---
+++ src/index.js
@@ -0,0 +1,114 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+const isDOMReady = !!((typeof window !== 'undefined' && window.document && window.document.createElement));
+
+const MethodList = ["identify", "goal", "updateUserData", "start", "stop", "refresh", "show", "hide", "on"];
+
+
+/**
+ * Shared vars
+ *
+ * */
+const SignalayerAPI = {};
+const BeforeLoadAPIRequests = [];
+
+/**
+ * Temp API methods (until player API loaded)
+ *
+ * */
+MethodList.forEach((methodName) => {
+    SignalayerAPI[methodName] = function (method) {
+        return function () {
+            const args = Array.prototype.slice.call(arguments);
+            BeforeLoadAPIRequests.push({method: method, args: args})
+        }
+    }(methodName)
+});
+
+
+export {SignalayerAPI};
+
+
+/**
+ * Signalayer Main Component player.js
+ *
+ * */
+
+export default class SignalayerPlayer extends Component {
+    static propTypes = {
+        projectId: PropTypes.string.isRequired,
+        userData: PropTypes.object,
+        async: PropTypes.bool
+    };
+
+    static displayName = 'Signalayer';
+
+    constructor(props) {
+        super(props);
+
+        const {
+            projectId,
+            userData,
+            async,
+            ...otherProps,
+        } = props;
+
+        if (!projectId || !isDOMReady) {
+            return;
+        }
+
+        if (!window.Signalayer) {
+            ((w, t, e) => {
+                w.Signalayer = {
+                    _apiKey: projectId,
+                    cs: BeforeLoadAPIRequests
+                };
+
+                let n = t.createElement(e);
+                let s = t.getElementsByTagName(e)[0];
+
+                n.src = "https://cdn.signalayer.com/static/player.js";
+                n.type = "text/javascript";
+                n.async = (typeof async !== 'undefined') ? async : true;
+                s.parentNode.insertBefore(n, s);
+            })(window, document, "script");
+        }
+
+        if (userData) window.SignalayerUserData = userData;
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const {
+            projectId,
+            userData,
+            ...otherProps,
+        } = nextProps;
+
+        if (!isDOMReady) return;
+
+        if (!window.Signalayer) {
+            if (userData) window.SignalayerUserData = userData;
+        } else {
+
+        }
+    }
+
+    shouldComponentUpdate() {
+        return false;
+    }
+
+    componentWillUnmount() {
+        if (!isDOMReady || !window.Signalayer) return false;
+
+        window.Signalayer.stop();
+
+        delete window.Signalayer;
+        delete window.SignalayerUserData;
+    }
+
+
+    render() {
+        return false;
+    }
+}
